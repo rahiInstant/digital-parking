9e33875ae7cfee8b1d6a064c09ea6dfc02320f4f
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-09-16 20:08:24 +0600

    first commit

---
+++ safe-park/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
+
+
+/**
+ * TODO 1: Sign in/up 
+ * TODO 2: Parking Entry
+ * TODO 3: User search 
+ */
